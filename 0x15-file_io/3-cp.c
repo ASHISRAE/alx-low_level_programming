be0ed1932ab541f2107b7cef686097b7a0bfa852
ASHISRAE <125803037+ASHISRAE@users.noreply.github.com>
2023-05-26 17:51:21 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,88 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
+
+void cant_close(int fd);
+void cant_write(char *file_to);
+void cant_read(char *file_from);
 
 /**
- * main - program that copies content of a file to another file
- * @argc: number of arguments passed
- * @argv: arguments
- *
- * Return: Always 0 (success)
+ * main - afunction to cp one file into another
+ * @argc: the number of args we will be passed
+ * @argv: an array of arguments
+ * Return: 0 for success
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	int src, dest, wr, re = 1024;
-	char buffer[1024];
+	int fd1, fd2;
+	ssize_t rd, wrt;
+	char *buf[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	if (argv[1] == NULL)
+		cant_read(argv[1]);
 
-	src = open(argv[1], O_RDONLY);
-	if (src < 0)
-		err_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (dest < 0)
-		err_exit(99, "Error: Can't write to %s\n", argv[2]);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		cant_read(argv[1]);
 
-	while (re == 1024)
-	{
-		re = read(src, buffer, 1024);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+		cant_write(argv[2]);
 
-		if (re < 0)
-			err_exit(98, "Error: Can't read from file %s\n", argv[1]);
-		wr = write(dest, buffer, re);
-		if (wr < 0)
-			err_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
-	if (close(src) < 0)
+	rd = read(fd1, buf, 1024);
+	if (rd == -1)
+		cant_read(argv[1]);
+	wrt = write(fd2, buf, rd);
+	if (wrt == -1)
+		cant_write(argv[2]);
+
+	while (rd == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
-		exit(100);
+		rd = read(fd1, buf, 1024);
+		if (rd == -1)
+			cant_read(argv[1]);
+
+		wrt = write(fd2, buf, rd);
+		if (wrt == -1)
+			cant_write(argv[2]);
 	}
-	if (close(dest) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest);
-		exit(100);
-	}
+
+	if (close(fd1) < 0)
+		cant_close(fd1);
+	if (close(fd2) < 0)
+		cant_close(fd2);
+
 	return (0);
 }
 /**
- * err_exit - prints error messages to POSIX standard error
- * @code: exit status
- * @erm: error message
- * @argument: name of file causing the error
- *
+ * cant_read - a function to print an error and exit
+ * @file_from: a pointer to our originalfile
  */
-
-void err_exit(int code, char *erm, char *argument)
+void cant_read(char *file_from)
 {
-	dprintf(STDERR_FILENO, erm, argument);
-	exit(code);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+	exit(98);
+}
+/**
+ * cant_write - a function to print an error message and exit
+ * @file_to: a pointer to our receiving file
+ */
+void cant_write(char *file_to)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+	exit(99);
+}
+
+/**
+ * cant_close - a function to print out an error and exit
+ * @fd: our file descriptor
+ */
+void cant_close(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
